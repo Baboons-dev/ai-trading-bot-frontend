2fcdd321e1fb66c28145fbffaca683246a424932
Bernard Namangala <58230307+Bernard-Namangala@users.noreply.github.com>
2024-12-05 20:15:33 +0200

    update app id

--- context/ClientProvider.tsx
+++ context/ClientProvider.tsx
@@ -1,35 +1,35 @@
-"use client";
-import dynamic from "next/dynamic";
-import { QueryClient, QueryClientProvider } from "react-query";
-import styled, { ThemeProvider } from "styled-components";
-import { AppTheme } from "@/components/getCsprTheme";
-import "prismjs";
-import { ThemeModeType, TopBarSettings } from "@make-software/csprclick-ui";
-import "prismjs/themes/prism-okaidia.css";
-import "prismjs/components/prism-typescript";
-import { NETWORKS } from "@/components/settings";
-import { useState } from "react";
+'use client';
+import dynamic from 'next/dynamic';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import styled, { ThemeProvider } from 'styled-components';
+import { AppTheme } from '@/components/getCsprTheme';
+import 'prismjs';
+import { ThemeModeType, TopBarSettings } from '@make-software/csprclick-ui';
+import 'prismjs/themes/prism-okaidia.css';
+import 'prismjs/components/prism-typescript';
+import { NETWORKS } from '@/components/settings';
+import { useState } from 'react';
 
 const ClickProvider = dynamic(
-  () => import("@make-software/csprclick-ui").then((mod) => mod.ClickProvider),
+  () => import('@make-software/csprclick-ui').then((mod) => mod.ClickProvider),
   {
     ssr: false,
-  }
+  },
 );
 
 const ClickUI = dynamic(
-  () => import("@make-software/csprclick-ui").then((mod) => mod.ClickUI),
+  () => import('@make-software/csprclick-ui').then((mod) => mod.ClickUI),
   {
     ssr: false,
-  }
+  },
 );
 
 const BuyCSPRMenuItem = dynamic(
   () =>
-    import("@make-software/csprclick-ui").then((mod) => mod.BuyCSPRMenuItem),
+    import('@make-software/csprclick-ui').then((mod) => mod.BuyCSPRMenuItem),
   {
     ssr: false,
-  }
+  },
 );
 
 const queryClient = new QueryClient({
@@ -37,35 +37,35 @@ const queryClient = new QueryClient({
 });
 
 const clickOptions = {
-  appName: "CSPR.click",
-  contentMode: "iframe",
+  appName: 'CSPR.click',
+  contentMode: 'iframe',
   providers: [
-    "casper-wallet",
+    'casper-wallet',
     // "ledger",
     // "casperdash",
     // "metamask-snap",
     // "torus-wallet",
     // "casper-signer",
   ],
-  appId: "csprclick-template",
+  appId: 'e0ae2f89-016c-4a72-b9a1-f28fc300',
 };
 
 const TopBarSection = styled.section(({ theme }) => ({
   backgroundColor: theme.topBarBackground,
-  position: "fixed",
+  position: 'fixed',
   zIndex: 1,
-  width: "100%",
+  width: '100%',
 }));
 
 const TopBarContainer = styled.div(({ theme }) =>
   theme.withMedia({
-    display: "flex",
-    flexDirection: "row",
-    width: "100%",
-    maxWidth: ["540px", "720px", "960px"],
-    margin: "0 auto",
-    padding: "0 12px",
-  })
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    maxWidth: ['540px', '720px', '960px'],
+    margin: '0 auto',
+    padding: '0 12px',
+  }),
 );
 
 export default function ClientProvider({
@@ -73,17 +73,17 @@ export default function ClientProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const accountMenuItems = [<BuyCSPRMenuItem key={"buy-cspr"} />];
+  const accountMenuItems = [<BuyCSPRMenuItem key={'buy-cspr'} />];
   const [network, setNetwork] = useState<string>(NETWORKS[1]);
   const [themeMode, setThemeMode] = useState<ThemeModeType>(
-    ThemeModeType.light
+    ThemeModeType.light,
   );
 
   const handleThemeSwitch = () => {
     setThemeMode(
       themeMode === ThemeModeType.light
         ? ThemeModeType.dark
-        : ThemeModeType.light
+        : ThemeModeType.light,
     );
   };
 
